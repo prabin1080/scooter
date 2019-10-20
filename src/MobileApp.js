720a4446e19b38ffa1cb8dee8a28368ecc391541
Prabin Kumar Sahu <prabin@determinantstudios.com>
2019-10-20 09:25:17 +0530

    refactor: modified towards mobile app style

--- src/MobileApp.js
+++ src/MobileApp.js
@@ -1,9 +1,23 @@
 import React from "react";
 import axios from "axios";
 import base_url from "./api_config";
+import "./MobileApp.css";
 
 class MobileApp extends React.Component {
-	state = { qr_code: "", is_scooter_rented: false, booking_count: 0 };
+	state = {
+		qr_code: "",
+		is_scooter_rented: false,
+		count: 0,
+		notification: null
+	};
+
+	showNotification = text => {
+		this.setState({ notification: text });
+		clearInterval(this.interval);
+		this.interval = setInterval(() => {
+			this.setState({ notification: null });
+		}, 3000);
+	};
 
 	handleQrChange = e => {
 		this.setState({ qr_code: e.target.value });
@@ -14,6 +28,7 @@ class MobileApp extends React.Component {
 			.get(base_url + "/zone/booking-details/", {})
 			.then(res => {
 				console.log(res);
+				console.log(res.data);
 				this.setState({ ...res.data });
 			})
 			.catch(error => {
@@ -31,14 +46,14 @@ class MobileApp extends React.Component {
 			.then(res => {
 				console.log(res);
 				this.setState({ qr_code: "" });
-				if (res.data.hasOwnProperty("booking_count")) {
-					this.setState({ booking_count: res.data.booking_count });
+				if (res.data.hasOwnProperty("count")) {
+					this.setState({ count: res.data.count });
 				}
-				alert(res.data.message);
+				this.showNotification(res.data.message);
 			})
 			.catch(error => {
 				console.log(error);
-				alert("There is an error in updating zone");
+				this.showNotification("There is an error in updating zone");
 			});
 	};
 
@@ -50,14 +65,15 @@ class MobileApp extends React.Component {
 			.then(res => {
 				console.log(res);
 				this.setState({ is_scooter_rented: true });
-				if (res.data.hasOwnProperty("booking_count")) {
-					this.setState({ booking_count: res.data.booking_count });
+				this.showNotification("Your Ride has Started");
+				if (res.data.hasOwnProperty("count")) {
+					this.setState({ count: res.data.count });
 				}
 				// alert(res.data.message);
 			})
 			.catch(error => {
 				console.log(error);
-				alert("There is an error");
+				this.showNotification(error.response.data.message);
 			});
 	};
 
@@ -69,39 +85,56 @@ class MobileApp extends React.Component {
 			.then(res => {
 				console.log(res);
 				this.setState({ is_scooter_rented: false });
-				if (res.data.hasOwnProperty("booking_count")) {
-					this.setState({ booking_count: res.data.booking_count });
+				this.showNotification("Your Ride has Ended");
+				if (res.data.hasOwnProperty("count")) {
+					this.setState({ count: res.data.count });
 				}
 				// alert("Ride Ended");
 			})
 			.catch(error => {
-				console.log(error);
-				alert("There is an error");
+				console.log(error.response.data);
+				this.showNotification(error.response.data.message);
 			});
 	};
 	render() {
 		return (
-			<div className="MobileApp">
+			<div className="mobileApp">
 				<h1>MobileApp</h1>
 				<hr />
-				<h4>Currently you have {this.state.booking_count} helmets</h4>
-				{this.state.is_scooter_rented ? (
-					<div>
-						<div>You have rented a bike</div>
-						<button onClick={this.handleEndRide}>End Ride</button>
+				<div className="mobileBox">
+					{this.state.notification ? (
+						<div className="notification">
+							{this.state.notification}
+						</div>
+					) : null}
+					<div className="rideSection">
+						{this.state.is_scooter_rented ? (
+							<div>
+								<div>You have booked a Vogo</div>
+								<button onClick={this.handleEndRide}>
+									End Ride
+								</button>
+							</div>
+						) : (
+							<button onClick={this.handleStartRide}>
+								Start Ride
+							</button>
+						)}
 					</div>
-				) : (
-					<button onClick={this.handleStartRide}>Start Ride</button>
-				)}
-				<form onSubmit={this.handleSubmit} style={{ marginTop: 100 }}>
-					<input
-						type="text"
-						value={this.state.qr_code}
-						onChange={this.handleQrChange}
-					/>
-					<button>Send QR Data</button>
-					<br />
-				</form>
+					<form
+						onSubmit={this.handleSubmit}
+						style={{ marginTop: 100 }}
+					>
+						<input
+							type="text"
+							value={this.state.qr_code}
+							onChange={this.handleQrChange}
+						/>
+						<button>Send QR Data</button>
+						<br />
+					</form>
+					<h4>Currently you have {this.state.count} helmets</h4>
+				</div>
 			</div>
 		);
 	}
